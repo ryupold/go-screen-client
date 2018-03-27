b9a18ea90401e9bf63f7e0e012b3ad72ee32c1a8
Michael Scherbakow <ryupold@gmail.com>
2018-03-28 01:35:17 +0200

    added logging

--- client.go
+++ client.go
@@ -16,34 +16,67 @@ func startStreaming(ip string, port uint16) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	con, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
 	if err != nil {
+		cancel()
 		return err
 	}
 
+	errChan := make(chan error)
+
 	go func() {
+		defer close(errChan)
 		defer cancel()
+		defer func() {
+			pancake := recover()
+			if pancake == nil {
+				return
+			}
+			paniC, ok := pancake.(error)
+			if ok {
+				errChan <- paniC
+			}
+		}()
+
 		for {
 			img, err := screenshot.CaptureScreen()
 			if err != nil || img == nil {
-				panic(err)
+				errChan <- err
+				return
 			}
+			log("screen captured")
+
 			buffer := &bytes.Buffer{}
 			if err := jpeg.Encode(buffer, image.Image(img), &jpeg.Options{Quality: 100 /*default: 75*/}); err != nil {
-				fmt.Printf("ERROR: %+v", err)
+				errChan <- err
+				return
 			}
+			log("jpeg encoded")
 
 			sizeBytes := make([]byte, 4)
 			binary.BigEndian.PutUint32(sizeBytes, uint32(buffer.Len()))
+			log("size calculated")
 
 			n, err := con.Write(append(sizeBytes, buffer.Bytes()...))
 			if err != nil {
-				panic(err)
+				errChan <- err
+				return
 			}
-			if n != buffer.Len() {
-				panic(fmt.Errorf("%d != %d written bytes differ", n, buffer.Len()))
+
+			log("bytes sent")
+			if n != buffer.Len()+4 /*4 bytes -> img size*/ {
+				errChan <- fmt.Errorf("%d != %d written bytes differ", n, buffer.Len())
+				return
 			}
 		}
 	}()
 
-	<-ctx.Done()
-	return nil
+	select {
+	case <-ctx.Done():
+		return nil
+	case err := <-errChan:
+		return err
+	}
+}
+
+func log(s string) {
+	fmt.Println(s)
 }
